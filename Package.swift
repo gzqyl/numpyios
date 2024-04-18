9082e2336dd365c9a500e83fe510ccbcfd7fffe7
allenyan <allen.yan7@foxmail.com>
2024-04-18 22:23:18 +0800

    update numpy-ios

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["libnumpy", "libnpymath", "libnpyrandom", "LinkNumpy", "NumPySupport"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/gzqyl/pyios", .branch("master")),
+        .package(name: "Python-iOS", url: "https://github.com/gzqyl/pyios", .branch("master")),
     ],
     targets: [
         .binaryTarget(name: "libnumpy", url: "https://github.com/gzqyl/numpyios/releases/download/binaryxc/libnumpy.xcframework.zip", checksum: "99ae0916f33840cf9c97b423147e00c8ec5e0eb5e68d1a36731f230309d9d40c"),
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(name: "libnpyrandom", url: "https://github.com/gzqyl/numpyios/releases/download/binaryxc/libnpyrandom.xcframework.zip", checksum: "11527be142959412b036b43cc4b93c8fea29802bb4652f1ce561e97c93bf0a99"),
         .target(
             name: "LinkNumpy",
-            dependencies: ["libnumpy", "libnpymath", "libnpyrandom", "pyios"],
+            dependencies: ["libnumpy", "libnpymath", "libnpyrandom", "Python-iOS"],
             linkerSettings: [.linkedFramework("Accelerate")]),
         .target(
             name: "NumPySupport",
